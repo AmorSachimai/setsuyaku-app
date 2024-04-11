6576bb72316595c1d55e82426b574752c7b91ba0
uk454 <92313867+ukasama47@users.noreply.github.com>
2024-04-11 22:48:59 +0900

    Update Firebase.js
    API情報は秘密なので消しました

--- firebase/Firebase.js
+++ firebase/Firebase.js
@@ -3,13 +3,7 @@
 // import "firebase/firestore";
 
 // const firebaseConfig = {
-//   apiKey: "process.env.AIzaSyCvAD55Vd5-ub7SfKVr_2kFAzOFj0zQ0KU",
-//   authDomain: " process.env.setsuyaku-app-component.firebaseapp.com",
-//   databaseURL: "process.env.https://setsuyaku-app-component.firebaseio.com",
-//   projectId: "process.env.setsuyaku-app-component",
-//   storageBucket: "process.env.setsuyaku-app-component.appspot.com",
-//   messagingSenderId: "process.env.884270687130",
-//   //appId: "setsuyaku-app-component",
+//ここは秘密だよ
 // };
 
 // const firebaseApp = firebase.initializeApp(firebaseConfig);
