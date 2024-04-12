96f498c104cc253a40d992b4add1d1e2d26716a3
ukasama47 <sfb01118@st.osakafu-u.ac.jp>
2024-04-12 18:47:40 +0900

    firebase情報を隠す

--- firebase/Firebase.js
+++ firebase/Firebase.js
@@ -9,13 +9,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCvAD55Vd5-ub7SfKVr_2kFAzOFj0zQ0KU",
-  authDomain: "setsuyaku-app-component.firebaseapp.com",
-  projectId: "setsuyaku-app-component",
-  storageBucket: "setsuyaku-app-component.appspot.com",
-  messagingSenderId: "884270687130",
-  appId: "1:884270687130:web:e697fdcd97ebc6563dd023",
-  measurementId: "G-GGMLW2R76S"
+//discordにあるよ
 };
 
 // Initialize Firebase
