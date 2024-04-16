769d042f4956346ffdce4b3ebc9d01d573c30975
ukasama47 <sfb01118@st.osakafu-u.ac.jp>
2024-04-16 17:21:29 +0900

    途中まで

--- App.js
+++ App.js
@@ -1,49 +1,98 @@
 //import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 //import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Home2 from "./components2/Home2";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator,} from "@react-navigation/native-stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 //import { CardStyleInterpolators} from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import * as React from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import React, { useEffect, useState } from "react";import app from "./firebase/Firebase";
+
+
+
+import { getAuth } from "firebase/auth";
+
+
+const auth = getAuth(app);
+
+
 
 
 export default function App() {
-
   const Stack = createNativeStackNavigator();
   const Tab = createBottomTabNavigator();
+  const [user, setUser] = useState('');
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      if (user) {
+        console.log(user);
+        setUser(user);
+      } else {
+        setUser('');
+      }
+    });
+    return () => unsubscribe();
+  }, []);
 
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        // initialRouteName="Home"
-        screenOptions={{
-          headerStyle: { backgroundColor: "#ADD8E6" },
-        //   headerTitleStyle: { color: "#fff" },
-        //   headerTintColor: "#fff",
-        //   headerBackTitle: "Back",
-        //   /* 画面遷移のアニメーションを指定することができる */
-        //   cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-        //   /* 画面をスワイプすると戻ることができる設定 Android用 iphoneはデフォルトでできる */
-        //   gestureEnavled: true,
-        //   gestureDirection: "horizontal",
+  if (loading) {
+    return (
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          flex: 1,
         }}
       >
-        <Tab.Screen name="ホーム" component={Home} />
-        <Tab.Screen name="あ" component={Home2} />
-      </Tab.Navigator>
+        <Text>Loading...</Text>
+      </View>
+    );
+  } else {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {user ? (
+          <Tab.Navigator
+            // initialRouteName="Home"
+            screenOptions={{
+              headerStyle: { backgroundColor: "#ADD8E6" },
+              //   headerTitleStyle: { color: "#fff" },
+              //   headerTintColor: "#fff",
+              //   headerBackTitle: "Back",
+              //   /* 画面遷移のアニメーションを指定することができる */
+              //   cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+              //   /* 画面をスワイプすると戻ることができる設定 Android用 iphoneはデフォルトでできる */
+              //   gestureEnavled: true,
+              //   gestureDirection: "horizontal",
+            }}
+          >
+            <Tab.Screen name="ホーム" component={Home} />
+            <Tab.Screen name="あ" component={Home2} />
+          </Tab.Navigator>
+        ) : (
+          <>
+            {/* まだわからないことがある */}
+              <Stack.Screen name="ログイン" component={Login} />
+              <Stack.Screen name="登録" component={Register} />
+            
+          </>
+        )}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
